66e1227c2c0c4ede4601dff43ec5c215483d2312
Aleksandr Ivanov <sasha942710@yandex.ru>
2024-07-28 17:25:43 +0300

    Основа приложения для котиков

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class Post { //  модель, описывающая сообщения в социальной сети. У неё следующие поля данных:
+
+    Long id; // уникальный идентификатор сообщения
+    long authorId; // пользователь, который создал сообщение,
+    String description;  //  текстовое описание сообщения
+    Instant postDate; // дата и время создания сообщения
+}
