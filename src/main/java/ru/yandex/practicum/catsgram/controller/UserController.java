c3ecc5f4864bd432728b0051e21cd6070b28f904
Aleksandr Ivanov <sasha942710@yandex.ru>
2024-08-06 19:33:19 +0300

    Добавили работу с сервером

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,49 @@
+package ru.yandex.practicum.catsgram.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public Collection<User> getUserList() {
+        return userService.getUserList();
+    }
+
+    @GetMapping("{id}")
+    public Optional<User> findUserById(@PathVariable("id") Long userById){
+        return userService.findUserById(userById);
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+
+        return userService.updateUser(user);
+    }
+}
